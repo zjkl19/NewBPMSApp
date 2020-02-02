f4a50112fb619b607584be408bfd6741b0848351
迪南 林 <eamdfan@hotmail.com>
2020-02-02 15:18:48 +0800

    添加项目文件。

---
+++ NewBPMSApp/NewBPMSApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NewBPMSApp.Models;
+
+namespace NewBPMSApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
